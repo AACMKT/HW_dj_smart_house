d635464032b2406dcfa50bc20d299315e6e2b452
Maxim E <maermolenko@mail.ru>
2023-08-23 19:19:39 +0300

    Updated urls

--- measurement/urls.py
+++ measurement/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from measurement.views import MeasurementsCreate, ViewEditSensor, SensorsListCreate
 
 urlpatterns = [
-    path('sensors', SensorsListCreate.as_view(), name='AddListSensors'),
-    path('measurements', MeasurementsCreate.as_view(), name='AddMeasurement'),
-    path('sensors/<pk>', ViewEditSensor.as_view(), name='EditListSensor'),
+    path('sensors/', SensorsListCreate.as_view(), name='AddListSensors'),
+    path('measurements/', MeasurementsCreate.as_view(), name='AddMeasurement'),
+    path('sensors/<pk>/', ViewEditSensor.as_view(), name='EditListSensor'),
 ]
